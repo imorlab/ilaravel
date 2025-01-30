94397672cf0f42d310d487c94fe58a7cf958eb78
Israel Moreno <iml@beonww.com>
2025-01-30 11:21:24 +0100

    Update email template heights and adjust Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
-                'resources/js/card-effects.js'
+                'resources/js/card-effects.js',
+                'resources/js/sweetalert.js'
             ],
             refresh: true,
         }),
